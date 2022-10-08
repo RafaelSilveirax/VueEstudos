f42724a8989676252dcef5901ea108741e133743
Rafael Silveira <rafaelsilveira2011@live.com>
2022-10-08 16:33:54 -0300

    Aula 6

--- CursoVueTiagoMatos/app/src/App.vue
+++ CursoVueTiagoMatos/app/src/App.vue
@@ -1,15 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div v-for="(obj, index) in todos" v-bind:key="obj.id">
+      <img
+      v-if="obj.imgScr"
+        v-bind:src="obj.imgScr"
+       >
+      <span>{{index}}</span> - {{obj.title}}
+    </div>
+
+    <TheHeader
+      v-if="showHeader"
+    />
+
+    <div v-show="showName">
+      Nome: {{name}}
+    </div>
+
+    <div v-if="accessLevel === 'admin'">Admin</div>
+    <div v-else-if="accessLevel === 'Markting'">Markting</div>
+    <div v-else>User</div>
+
+    <img 
+      alt="Vue logo" 
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheHeader
+  },
+  data(){
+    return{
+      showHeader: true,
+      name: 'eu te amo',
+      showName: false,
+      accessLevel: 'admin',
+      todos:[
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false,
+          "imgScr": "https://via.placeholder.com/150",
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false,
+          "imgScr": "https://via.placeholder.com/150",
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ CursoVueTiagoMatos/app/src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        Hearder
+    </header>
+</template>
+
+<script>
+
+    export default {
+        name: "TheHeader",
+
+    }
+
+</script>
+
+
+<style>
+    header{
+        background: black;
+        color: white;
+    }
+</style>
